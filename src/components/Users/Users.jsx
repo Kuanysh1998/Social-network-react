552df4365b959cc84dd52c457501ceb86bfee257
Kuanysh <kuanysh1998@gmail.com>
2021-04-06 00:17:46 +0600

    replaced functional component Users with class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,26 +1,29 @@
 import * as axios from "axios";
+import React from "react";
 import classes from "./Users.module.css";
+import noPhoto from "../../assets/images/unnamed.png"
 
-
-
-function Users(props) {
-
-    function getUsers(){
-        if(props.usersData.length === 0){
+class Users extends React.Component {
+   
+    componentDidMount(){
+        if(this.props.usersData.length === 0){
             axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response=>{props.setUsers(response.data.items)})
-    }
+                .then(response=>{this.props.setUsers(response.data.items)})
     
-       
-}
-    return <div>
-        <button onClick = {getUsers}>Get users</button>
-        {props.usersData.map(u => <div key={u.id}>
+        }
+    }
+
+
+    render() {
+
+        return <div>
+
+        {this.props.usersData.map(u => <div key={u.id}>
             <span>
-                <div><img className={classes.avatar} src={u.photos.small != null ? u.photos.small : "https://lh3.googleusercontent.com/proxy/ZyKtQ5q2XuuEyAIHIi7rsG2Lk43dQ_siGvvjgz_3iK-W9acK3rxUfcy588AaPNKcxI6zhNz9vNbecsr4qEGv3ln8hmcvc9VcMr2-av5h"} /></div>
+                <div><img className={classes.avatar} src={u.photos.small != null ? u.photos.small : noPhoto} /></div>
                 <div>{u.followed === true ?
-                    <button onClick={() => { props.unfollow(u.id) }}>unfollow</button>
-                    : <button onClick={() => { props.follow(u.id) }}>follow</button>
+                    <button onClick={() => { this.props.unfollow(u.id) }}>unfollow</button>
+                    : <button onClick={() => { this.props.follow(u.id) }}>follow</button>
 
                 }</div>
             </span>
@@ -36,6 +39,7 @@ function Users(props) {
         }
     
     </div>
+    }
 }
-debugger;
+
 export default Users;
